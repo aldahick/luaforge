f025716f2374db6a4e8b467d7b58ccbc03550cbc
samrg472 <samrg472>
2012-10-21 21:38:37 -0400

    Added load method

--- src/common/tiin57/luaforge/core/Core.java
+++ src/common/tiin57/luaforge/core/Core.java
@@ -4,7 +4,9 @@ import java.io.File;
 
 import net.minecraft.client.Minecraft;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
@@ -40,4 +42,10 @@ public class Core {
 			}
 		}
 	}
+    
+    @Init
+    public void load(FMLInitializationEvent event) {
+        Log.info("Sucessfully loaded");
+    }
+    
 }
