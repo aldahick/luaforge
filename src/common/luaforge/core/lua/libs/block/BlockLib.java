2971774c39be7696c3fd1b0c540114b1bc8c144a
tiin57 <alex@vilnis.com>
2012-10-28 17:31:34 -0400

    Merge branch 'master' of https://github.com/tiin57/LuaForge
    Conflicts:
    	src/common/luaforge/core/lua/libs/block/BlockLib.java
    	src/common/luaforge/core/lua/libs/block/BlockTemplate.java

--- src/common/luaforge/core/lua/libs/block/BlockLib.java
+++ src/common/luaforge/core/lua/libs/block/BlockLib.java
@@ -1,20 +1,71 @@
 package luaforge.core.lua.libs.block;
 
-import luaforge.core.api.LuaMethod;
-import org.luaj.vm2.LuaValue;
+import java.io.File;
+
+import net.minecraft.src.Material;
+
 import org.luaj.vm2.Varargs;
 
+import luaforge.core.api.LuaMethod;
+
 public class BlockLib {
-
-    @LuaMethod(name = "block")
+    
+    @LuaMethod (name = "block")
     public static Varargs createBlock(Varargs args) {
-        // TODO: Finish this
-        return LuaValue.NONE;
+    	int numberOfArgs = args.narg();
+    	Object[] arg = new Object[] {};
+    	for (int i=0; i<numberOfArgs; i++) {
+    		arg[i] = (Object) args.arg(i);
+    	}
+    	int id = (Integer) arg[0];
+    	int iconIndex = (Integer) arg[1];
+    	String mString = arg[2].toString();
+    	Material material = null;
+    	Material[] materials = new Material[] {
+    		Material.air,
+    		Material.cactus,
+    		Material.cake,
+    		Material.circuits,
+    		Material.clay,
+    		Material.cloth,
+    		Material.craftedSnow,
+    		Material.dragonEgg,
+    		Material.fire,
+    		Material.glass,
+    		Material.grass,
+    		Material.ground,
+    		Material.ice,
+    		Material.iron,
+    		Material.lava,
+    		Material.leaves,
+    		Material.piston,
+    		Material.plants,
+    		Material.portal,
+    		Material.pumpkin,
+    		Material.redstoneLight,
+    		Material.rock,
+    		Material.sand,
+    		Material.snow,
+    		Material.sponge,
+    		Material.tnt,
+    		Material.vine,
+    		Material.water,
+    		Material.web,
+    		Material.wood
+    	};
+    	for (int i=0; i<materials.length; i++) {
+    		if (mString.equals(materials[i].toString())) {
+    			material = materials[i];
+    		}
+    	}
+    	String name = arg[3].toString();
+    	String textureFileString = arg[4].toString();
+    	String[] otherArgs = new String[] {
+    		name,
+    		textureFileString
+    	};
+    	new BlockTemplate(id, iconIndex, material, otherArgs);
+		return args;
     }
-
-    @LuaMethod(name = "block")
-    public static Varargs createBlockContainer(Varargs args) {
-        // TODO: Create class BlockContainerTemplate
-        return LuaValue.NONE;
-    }
+    
 }
--- src/common/luaforge/core/lua/libs/block/BlockTemplate.java
+++ src/common/luaforge/core/lua/libs/block/BlockTemplate.java
@@ -7,29 +7,29 @@ import net.minecraft.src.StepSound;
 
 public class BlockTemplate extends Block {
 
-    private String textureFile;
+     private String textureFile;
 
-    public BlockTemplate(int id, int iconIndex, Material material, String[] otherArgs) {
-        super(id, iconIndex, material);
-        this.setBlockName(otherArgs[0]);
-        this.textureFile = otherArgs[1];
-    }
-
-    @Override
-    public String getTextureFile() {
-        return textureFile;
-    }
-
-    public void setTab(CreativeTabs tab) {
-        this.setCreativeTab(tab);
-    }
-
-    public void setSoundOnStep(StepSound sound) {
-        this.setStepSound(sound);
-    }
-
-    public void setHardnessResistance(float hardness, float resistance) {
-        this.setHardness(hardness);
-        this.setResistance(resistance);
-    }
+     public BlockTemplate (int id, int iconIndex, Material material, String[] otherArgs) {
+            super(id, iconIndex, material);
+            this.setBlockName(otherArgs[0]);
+            this.textureFile = otherArgs[1];
+     }
+     
+     @Override
+     public String getTextureFile() {
+           return textureFile;
+     }
+     
+     public void setTab(CreativeTabs tab){
+            this.setCreativeTab(tab);
+     }
+     
+     public void setSoundOnStep(StepSound sound) {
+           this.setStepSound(sound);
+     }
+     
+     public void setHardnessResistance(float hardness, float resistance) {
+           this.setHardness(hardness);
+           this.setResistance(resistance);
+     }
 }
