b79082c9052c16049e23b79a24eb101da0f2e26d
samrg472 <samrg472>
2012-10-28 16:15:03 -0400

    BlockLib is going good

--- src/common/luaforge/core/lua/libs/block/BlockLib.java
+++ src/common/luaforge/core/lua/libs/block/BlockLib.java
@@ -1,7 +1,20 @@
 package luaforge.core.lua.libs.block;
 
+import luaforge.core.api.LuaMethod;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.Varargs;
+
 public class BlockLib {
-    
-    
-    
+
+    @LuaMethod(name = "block")
+    public static Varargs createBlock(Varargs args) {
+        // TODO: Finish this
+        return LuaValue.NONE;
+    }
+
+    @LuaMethod(name = "block")
+    public static Varargs createBlockContainer(Varargs args) {
+        // TODO: Create class BlockContainerTemplate
+        return LuaValue.NONE;
+    }
 }
--- src/common/luaforge/core/lua/libs/block/BlockTemplate.java
+++ src/common/luaforge/core/lua/libs/block/BlockTemplate.java
@@ -1,5 +1,35 @@
 package luaforge.core.lua.libs.block;
 
-public class BlockTemplate {
+import net.minecraft.src.Block;
+import net.minecraft.src.Material;
+import net.minecraft.src.CreativeTabs;
+import net.minecraft.src.StepSound;
 
+public class BlockTemplate extends Block {
+
+    private String textureFile;
+
+    public BlockTemplate(int id, int iconIndex, Material material, String[] otherArgs) {
+        super(id, iconIndex, material);
+        this.setBlockName(otherArgs[0]);
+        this.textureFile = otherArgs[1];
+    }
+
+    @Override
+    public String getTextureFile() {
+        return textureFile;
+    }
+
+    public void setTab(CreativeTabs tab) {
+        this.setCreativeTab(tab);
+    }
+
+    public void setSoundOnStep(StepSound sound) {
+        this.setStepSound(sound);
+    }
+
+    public void setHardnessResistance(float hardness, float resistance) {
+        this.setHardness(hardness);
+        this.setResistance(resistance);
+    }
 }
