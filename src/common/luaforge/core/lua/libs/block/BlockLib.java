69504ab5ef2e0bfd0f9bddcc2ed3cece9da04a48
samrg472 <samrg472>
2012-11-10 14:09:39 -0500

    Added the creativeTabs and material table

--- src/common/luaforge/core/lua/libs/block/BlockLib.java
+++ src/common/luaforge/core/lua/libs/block/BlockLib.java
@@ -94,7 +94,7 @@ public class BlockLib {
     }
     
     @LuaTable
-    public static HashMap<String, LuaValue> material() { // TODO: Document on the wiki
+    public static HashMap<String, LuaValue> creativeTabs() { // TODO: Document on the wiki
         HashMap<String, LuaValue> table = new HashMap<String, LuaValue>();
         table.put("allSearch", LuaValue.valueOf("tabAllSearch"));
         table.put("block", LuaValue.valueOf("tabBlock"));
@@ -108,6 +108,42 @@ public class BlockLib {
         table.put("redstone", LuaValue.valueOf("tabRedstone"));
         table.put("tools", LuaValue.valueOf("tabTools"));
         table.put("transport", LuaValue.valueOf("tabTransport"));
+        return table;
+    }
+    
+    @LuaTable
+    public static HashMap<String, LuaValue> material() {
+        HashMap<String, LuaValue> table = new HashMap<String, LuaValue>();
+        table.put("air", LuaValue.valueOf("air"));
+        table.put("cactus", LuaValue.valueOf("cactus"));
+        table.put("cake", LuaValue.valueOf("cake"));
+        table.put("circuits", LuaValue.valueOf("circuits"));
+        table.put("clay", LuaValue.valueOf("clay"));
+        table.put("cloth", LuaValue.valueOf("cloth"));
+        table.put("craftedSnow", LuaValue.valueOf("craftedSnow"));
+        table.put("dragonEgg", LuaValue.valueOf("dragonEgg"));
+        table.put("fire", LuaValue.valueOf("fire"));
+        table.put("glass", LuaValue.valueOf("glass"));
+        table.put("grass", LuaValue.valueOf("grass"));
+        table.put("ground", LuaValue.valueOf("ground"));
+        table.put("ice", LuaValue.valueOf("ice"));
+        table.put("iron", LuaValue.valueOf("iron"));
+        table.put("lava", LuaValue.valueOf("lava"));
+        table.put("leaves", LuaValue.valueOf("leaves"));
+        table.put("piston", LuaValue.valueOf("piston"));
+        table.put("plants", LuaValue.valueOf("plants"));
+        table.put("portal", LuaValue.valueOf("portal"));
+        table.put("pumpkin", LuaValue.valueOf("pumpkin"));
+        table.put("redstoneLight", LuaValue.valueOf("redstoneLight"));
+        table.put("rock", LuaValue.valueOf("rock"));
+        table.put("sand", LuaValue.valueOf("sand"));
+        table.put("snow", LuaValue.valueOf("snow"));
+        table.put("sponge", LuaValue.valueOf("sponge"));
+        table.put("tnt", LuaValue.valueOf("tnt"));
+        table.put("vine", LuaValue.valueOf("vine"));
+        table.put("water", LuaValue.valueOf("water"));
+        table.put("web", LuaValue.valueOf("web"));
+        table.put("wood", LuaValue.valueOf("wood"));
         return table;
     }
     
