a56c7444cac4d83dbf7c6761932d5b7d98f92788
Cloudhunter <cloudhunter@btinternet.com>
2012-11-19 00:52:42 +0000

    Textures now work due to RenderEngine hackery. Fixed server side loading (you're welcome!)

--- src/common/luaforge/core/proxies/ClientProxy.java
+++ src/common/luaforge/core/proxies/ClientProxy.java
@@ -1,28 +1,90 @@
 package luaforge.core.proxies;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
 import javax.imageio.ImageIO;
+
+import org.luaj.vm2.LuaValue;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.src.RenderEngine;
 import net.minecraftforge.client.MinecraftForgeClient;
 
 public class ClientProxy extends CommonProxy {
 
     @Override
     public void registerRenderers() {
+    	
+    	InputStream inputStream = null;
+    	
+    	Class c = RenderEngine.class;
+    	
+    	RenderEngine render = Minecraft.getMinecraft().renderEngine;
+    	
+    	Field textureMapField = null;
+    	
+    	try {
+			textureMapField = c.getDeclaredField("textureMap");
+		} catch (SecurityException e1) {
+		} catch (NoSuchFieldException e1) {
+		}
+    	
+    	textureMapField.setAccessible(true);
+    	
+    	HashMap textureMap = null;
+		try 
+		{
+			textureMap = (HashMap) textureMapField.get(render);
+		} 
+		catch (IllegalArgumentException e1) 
+		{
+		} 
+		catch (IllegalAccessException e1) 
+		{
+		}
+    	
+		if (textureMap == null)
+		{
+			System.out.println("Error occured when getting the RenderEngine instance. Go bitch at the LuaForge devs!");
+		}
         
         for (String s : CommonProxy.TEXTURES) {
-            MinecraftForgeClient.preloadTexture(s);
+        	
+        	File file = new File(s);
+        	
+        	try {
+    			inputStream = new FileInputStream(file);
+    		} catch (FileNotFoundException e) {
+    			System.out.println("File not found when preloading textures - a Lua mod may have issues!");
+    			System.out.println("File was " + file.getPath());
+    		}
+        	
+        	
+        	
+        	if (inputStream != null)
+        	{
+                BufferedImage var2 = null;
+    			try {
+    				var2 = readTextureImage(inputStream);
+    			} catch (IOException e) {
+        			System.out.println("Error reading file when preloading textures - a Lua mod may have issues!");
+        			System.out.println("File was " + file.getPath());
+    			}
+    			
+    			if (var2 != null)
+    				textureMap.put(s, render.allocateAndSetupTexture(var2));
+        	}
+            
+            
+            
         }
-    }
-    
-    private int[] getImageContentsAndAllocate(BufferedImage par1BufferedImage)
-    {
-        int var2 = par1BufferedImage.getWidth();
-        int var3 = par1BufferedImage.getHeight();
-        int[] var4 = new int[var2 * var3];
-        par1BufferedImage.getRGB(0, 0, var2, var3, var4, 0, var2);
-        return var4;
     }
     
     private BufferedImage readTextureImage(InputStream par1InputStream) throws IOException {
