cfee1860a791b3a3f3a4a0bba62d545acf194a2c
samrg472 <samrg472>
2012-12-18 19:06:18 -0500

    Implement drink ability in items

--- src/common/luaforge/core/lua/libs/item/ItemTemplate.java
+++ src/common/luaforge/core/lua/libs/item/ItemTemplate.java
@@ -12,35 +12,38 @@ public class ItemTemplate extends Item {
     private String visibleName;
     private String hiddenName;
     private String textureFile;
-    
     public EnumAction action = EnumAction.none;
-    
-    /** Number of ticks to run while 'EnumAction'ing until result. */
+    /**
+     * Number of ticks to run while 'EnumAction'ing until result.
+     */
     public int itemUseDuration = 32;
-
-    /** The amount this food item heals the player. */
+    /**
+     * The amount this food item heals the player.
+     */
     private int healAmount = 0;
     private float saturationModifier = 0;
-
     /**
-     * If this field is true, the food can be consumed even if the player don't need to eat.
+     * If this field is true, the food can be consumed even if the player don't
+     * need to eat.
      */
     private boolean alwaysEdible = false;
-
     /**
-     * represents the potion effect that will occurr upon eating this food. Set by setPotionEffect
+     * represents the potion effect that will occurr upon eating this food. Set
+     * by setPotionEffect
      */
     private int potionId;
-
-    /** set by setPotionEffect */
+    /**
+     * set by setPotionEffect
+     */
     private int potionDuration;
-
-    /** set by setPotionEffect */
+    /**
+     * set by setPotionEffect
+     */
     private int potionAmplifier;
-
-    /** probably of the set potion effect occurring */
+    /**
+     * probably of the set potion effect occurring
+     */
     private float potionEffectProbability;
-    
     public int healthAmount = 0;
 
     public ItemTemplate(int id, int iconIndex, Object[] otherArgs) {
@@ -52,19 +55,19 @@ public class ItemTemplate extends Item {
         this.setIconIndex(iconIndex);
         this.setItemName(hiddenName);
     }
-    
+
     public void setHealAmount(int i) {
         this.healAmount = i;
     }
-    
+
     public void setSaturation(float f) {
         this.saturationModifier = f;
     }
-    
+
     public void setAlwaysEdible() {
         this.alwaysEdible = true;
     }
-    
+
     public String getVisibleName() {
         return visibleName;
     }
@@ -85,14 +88,16 @@ public class ItemTemplate extends Item {
 
     @Override
     public ItemStack onItemRightClick(ItemStack itemStack, World w, EntityPlayer player) {
-        if(this.action == EnumAction.eat) {
+        if (this.action == EnumAction.eat) {
             if (player.canEat(this.alwaysEdible)) {
                 player.setItemInUse(itemStack, this.getMaxItemUseDuration(itemStack));
             }
+        } else if (this.action == EnumAction.drink) {
+            player.setItemInUse(itemStack, this.getMaxItemUseDuration(itemStack));
         }
         return itemStack;
     }
-    
+
     @Override
     public int getMaxItemUseDuration(ItemStack par1ItemStack) {
         return this.itemUseDuration;
@@ -100,20 +105,24 @@ public class ItemTemplate extends Item {
 
     @Override
     public ItemStack onFoodEaten(ItemStack par1ItemStack, World world, EntityPlayer player) {
-        --par1ItemStack.stackSize;
-        player.getFoodStats().addStats(this.healAmount, this.saturationModifier);
-        world.playSoundAtEntity(player, "random.burp", 0.5F, world.rand.nextFloat() * 0.1F + 0.9F);
-        this.func_77849_c(par1ItemStack, world, player);
-        player.heal(healthAmount);
+        if (this.action == EnumAction.eat) {
+            --par1ItemStack.stackSize;
+            player.getFoodStats().addStats(this.healAmount, this.saturationModifier);
+            world.playSoundAtEntity(player, "random.burp", 0.5F, world.rand.nextFloat() * 0.1F + 0.9F);
+            this.func_77849_c(par1ItemStack, world, player);
+            player.heal(healthAmount);
+        } else if (this.action == EnumAction.drink) {
+            if (!player.capabilities.isCreativeMode) {
+                --par1ItemStack.stackSize;
+            }
+            return par1ItemStack.stackSize <= 0 ? new ItemStack(Item.bucketEmpty) : par1ItemStack;
+        }
         return par1ItemStack;
     }
-    
-    protected void func_77849_c(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
-    {
-        if (!par2World.isRemote && this.potionId > 0 && par2World.rand.nextFloat() < this.potionEffectProbability)
-        {
+
+    protected void func_77849_c(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
+        if (!par2World.isRemote && this.potionId > 0 && par2World.rand.nextFloat() < this.potionEffectProbability) {
             par3EntityPlayer.addPotionEffect(new PotionEffect(this.potionId, this.potionDuration * 20, this.potionAmplifier));
         }
     }
-
 }
